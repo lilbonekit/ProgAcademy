42b0c21d1784a10c52cb4716b248bc7e1ccd0b7e
Mykyta Sydorenko <mykyta.sydorenko@nure.ua>
2023-01-30 22:19:48 +0200

    rem media done

--- 08/js/script.js
+++ 08/js/script.js
@@ -1,20 +1,22 @@
 const togglePopUp = () => {
     const popUp = document.getElementById('burgerPopUp');
+    const popUpBtn = document.getElementById('hamburger__btn');
+    const overlayPopUp = document.getElementById('overlayPopUp');
     if (popUp.style.display === 'none') {
         popUp.style.display = 'block';
-
+        overlayPopUp.style.display = 'block';
+        popUpBtn.src = "img/hero/close.svg";
         setTimeout(() => {
             popUp.style.right = '0px';
             popUp.style.opacity = '1';
             popUp.style.transform = 'rotateZ(0)';
         }, 1);
-        
-        
     } else {
         setTimeout(() => {
             popUp.style.display='none';
         }, 300);
-        
+        overlayPopUp.style.display = 'none';
+        popUpBtn.src = "img/hero/charm_menu-hamburger.svg"
         popUp.style.right = '-600px';
         popUp.style.opacity = '0';
         popUp.style.transform = 'rotateZ(-90deg)';
